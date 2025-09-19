037684e0e7ed98a3e046b311dce8ba2ac449ee5b
onesixpc <mail.onesixpc@gmail.com>
2025-09-19 22:52:11 +0530

    day 1

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ console.log("Hello, World!");
 */
 
 // "use strict";
+// enables strict mode which helps catch common coding mistakes
+
+// variable declarations
 let x = 10;
 // let is block scoped
 const y = 20;
@@ -18,17 +21,18 @@ let num = 100; // number
 let bool = true; // boolean
 let n = null; // null
 let undef; // undefined
-let sym = Symbol("id"); // symbol   
 
 // non-primitive data types
 let obj = { name: "Alice", age: 25 }; // object
 let arr = [1, 2, 3, 4, 5]; // array
 let func = function() { return "I am a function"; }; // function
-let date = new Date(); // date
-let regex = /ab+c/; // regular expression
 
 // type checking
 console.log(typeof str); // string
 console.log(typeof num); // number
-console.log(typeof bool);   // boolean
+console.log(typeof bool); // boolean
 console.log(typeof n); // object (this is a known quirk in JavaScript)
+console.log(typeof undef); // undefined
+console.log(typeof null); // object
+console.log(typeof obj); // object
+console.log(typeof arr); // object
